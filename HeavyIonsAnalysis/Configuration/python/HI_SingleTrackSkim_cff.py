644ad0d67193060f455a4f0484cdeb90123430c7
mandrenguyen <matthew.nguyen@cern.ch>
2015-11-01 23:05:02 +0100

    Update HI_SingleTrackSkim_cff.py

--- HeavyIonsAnalysis/Configuration/python/HI_SingleTrackSkim_cff.py
+++ HeavyIonsAnalysis/Configuration/python/HI_SingleTrackSkim_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 # HLT jet trigger
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 hltTrackHI = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hltTrackHI.HLTPaths = ["HLT_HIFullTrack45_*"]
+hltTrackHI.HLTPaths = ["HLT_HIFullTrack45_v*"]
 hltTrackHI.throw = False
 hltTrackHI.andOr = True
 

e1312919adf419a1e0b2dc8927911efc884762e2
Matthew <Matthew.Nguyen@cern.ch>
2018-08-07 13:26:44 +0200

    Exclude taus, IVF and PF Met from Run2_2018_pp_on_AA era

--- RecoVertex/AdaptiveVertexFinder/python/inclusiveVertexing_cff.py
+++ RecoVertex/AdaptiveVertexFinder/python/inclusiveVertexing_cff.py
@@ -60,6 +60,7 @@ inclusiveCandidateVertexingCvsL = cms.Sequence(inclusiveCandidateVertexingCvsLTa
 
 from Configuration.Eras.Modifier_pp_on_XeXe_2017_cff import pp_on_XeXe_2017
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
-for e in [pp_on_XeXe_2017, pp_on_AA_2018]:
-    e.toModify(inclusiveVertexFinder, minHits = 10, minPt = 1.0)
-    e.toModify(inclusiveCandidateVertexFinderCvsL, minHits = 10, minPt = 1.0)
+pp_on_XeXe_2017.toModify(inclusiveVertexFinder, minHits = 10, minPt = 1.0)
+pp_on_AA_2018.toModify(inclusiveVertexFinder, minHits = 999, minPt = 999.0)
+pp_on_XeXe_2017.toModify(inclusiveCandidateVertexFinderCvsL, minHits = 10, minPt = 1.0)
+pp_on_AA_2018.toModify(inclusiveCandidateVertexFinderCvsL, minHits = 999, minPt = 999.0)

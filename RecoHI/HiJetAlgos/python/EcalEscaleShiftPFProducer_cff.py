ef2fe467998fc4986a3fee3531e53b49f2fad771
Matthew Nguyen <matthew.nguyen@cern.ch>
2019-03-17 20:36:22 +0100

    Updated ECAL scale values

--- RecoHI/HiJetAlgos/python/EcalEscaleShiftPFProducer_cff.py
+++ RecoHI/HiJetAlgos/python/EcalEscaleShiftPFProducer_cff.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 
 ecalShiftParticleFlow = cms.EDProducer('EcalEscaleShiftPFProducer',
-                                           src    = cms.InputTag('particleFlow'),
-                                           removePreshower = cms.bool(False), 
-                                           scaleEB = cms.double(1.01),  # SETME
-                                           scaleEE = cms.double(1.15),  # SETME
+                                       src    = cms.InputTag('particleFlow'),
+                                       removePreshower = cms.bool(False), 
+                                       scaleEB = cms.double(0.9928),  # SETME
+                                       scaleEE = cms.double(1.1537),  # SETME
 
 )
 

f8e665b4cbfcba08e19950dad0ad8ccde90ea099
mandrenguyen <Matthew.Nguyen@cer.ch>
2022-12-16 15:59:41 +0100

    add bfinder

---
+++ Bfinder/test/DnBfinder_to_Forest_94X.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+PATHTOTEST=$CMSSW_BASE/src/HeavyIonsAnalysis/JetAnalysis/test
+FORESTS=(runForestAOD_pp_DATA_94X runForestAOD_pp_MC_94X)
+RUNONMC=(False True)
+# DIFFPATH=("process.hltanalysisReco *" "process.hltanalysis * process.runAnalyzer *")
+INFILES=(
+    # "file:/afs/cern.ch/work/w/wangj/public/DoubleMuon/Run2017G-17Nov2017-v1/DEBD2F77-0E38-E811-8161-A4BF0108B83A.root"
+    "file:/afs/cern.ch/work/w/wangj/public/HIZeroBias2/Run2017G-17Nov2017-v1/141428A3-8F24-E811-B2A0-1CC1DE056008.root"
+    "file:/afs/cern.ch/work/w/wangj/public/BJPsiMM_TuneCUETP8M1_5p02TeV_pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV-v2/C00FF4F0-D421-E911-9F69-141877410E71.root"
+)
+
+cc=0
+for FOREST in ${FORESTS[@]}
+do
+##
+    cp ${PATHTOTEST}/${FOREST}.py ${PATHTOTEST}/${FOREST}_wDfinder.py
+
+    echo '
+#################### D/B finder ################# 
+AddCaloMuon = False 
+runOnMC = '${RUNONMC[cc]}' ## !!
+HIFormat = False 
+UseGenPlusSim = False 
+VtxLabel = "offlinePrimaryVerticesWithBS" 
+TrkLabel = "generalTracks" 
+useL1Stage2 = True
+HLTProName = "HLT"
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X 
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel, useL1Stage2, HLTProName)
+process.Dfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
+process.Dfinder.makeDntuple = cms.bool(True)
+process.Dfinder.tkPtCut = cms.double(0.5) # before fit
+process.Dfinder.tkEtaCut = cms.double(2.5) # before fit
+process.Dfinder.dPtCut = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0) # before fit
+process.Dfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05)
+process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
+process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+process.dfinder = cms.Path(process.DfinderSequence)
+
+' >> ${PATHTOTEST}/${FOREST}_wDfinder.py
+
+##
+    cp ${PATHTOTEST}/${FOREST}.py ${PATHTOTEST}/${FOREST}_wBfinder.py
+
+    echo '
+#################### D/B finder #################
+AddCaloMuon = False
+runOnMC = '${RUNONMC[cc]}' ## !!
+HIFormat = False
+UseGenPlusSim = False
+VtxLabel = "offlinePrimaryVerticesWithBS"
+TrkLabel = "generalTracks"
+useL1Stage2 = True
+HLTProName = "HLT"
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel, useL1Stage2, HLTProName)
+
+process.Bfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
+process.Bfinder.makeBntuple = cms.bool(True)
+process.Bfinder.tkPtCut = cms.double(0.2) # before fit
+process.Bfinder.jpsiPtCut = cms.double(0.0) # before fit
+process.Bfinder.bPtCut = cms.vdouble(2.0, 5.0, 5.0, 2.0, 2.0, 2.0, 5.0) # before fit
+process.Bfinder.Bchannel = cms.vint32(1, 0, 0, 1, 1, 1, 0)
+process.Bfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.10)
+process.Bfinder.svpvDistanceCut = cms.vdouble(2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 0.0)
+process.Bfinder.doTkPreCut = cms.bool(True)
+process.Bfinder.doMuPreCut = cms.bool(True)
+process.Bfinder.MuonTriggerMatchingPath = cms.vstring(
+    "HLT_HIL1DoubleMu0_v1")
+process.Bfinder.MuonTriggerMatchingFilter = cms.vstring(
+    "hltL1fL1sDoubleMu0L1Filtered0")
+process.p = cms.Path(process.BfinderSequence)
+
+' >> ${PATHTOTEST}/${FOREST}_wBfinder.py
+
+#
+    cp ${PATHTOTEST}/${FOREST}_wDfinder.py ${PATHTOTEST}/${FOREST}_onlyDfinder.py
+    cp ${PATHTOTEST}/${FOREST}_wBfinder.py ${PATHTOTEST}/${FOREST}_onlyBfinder.py
+
+    for ifile in ${PATHTOTEST}/${FOREST}_onlyBfinder.py ${PATHTOTEST}/${FOREST}_onlyDfinder.py
+    do
+        sed -i "/D\/B finder/i \\
+process.ana_step = cms.Path( \\
+    process.hltanalysis * \\
+    process.hiEvtAnalyzer * \\
+    # process.hltobject + \\
+    process.HiForest \\
+    ) \\
+" $ifile
+    done
+
+#
+    for ifile in ${PATHTOTEST}/${FOREST}_onlyBfinder.py ${PATHTOTEST}/${FOREST}_onlyDfinder.py ${PATHTOTEST}/${FOREST}_wBfinder.py ${PATHTOTEST}/${FOREST}_wDfinder.py
+    do
+        echo '
+###############################
+import FWCore.ParameterSet.VarParsing as VarParsing
+ivars = VarParsing.VarParsing('"'"'analysis'"'"')
+
+ivars.maxEvents = -1
+ivars.outputFile='"'"'HiForestAOD.root'"'"'
+ivars.inputFiles='"'${INFILES[cc]}'"'
+ivars.parseArguments() # get and parse the command line arguments
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(ivars.inputFiles)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(ivars.maxEvents)
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(ivars.outputFile))
+' >> $ifile
+    done
+##
+    cc=$((cc+1))
+done
---
+++ HeavyIonsAnalysis/JetAnalysis/test/runForestAOD_pp_MC_94X_wDfinder.py
@@ -0,0 +1,264 @@
+### HiForest Configuration
+# Collisions: pp
+# Type: MC
+# Input: AOD
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process('HiForest')
+process.options = cms.untracked.PSet()
+
+#####################################################################################
+# HiForest labelling info
+#####################################################################################
+
+process.load("HeavyIonsAnalysis.JetAnalysis.HiForest_cff")
+process.HiForest.inputLines = cms.vstring("HiForest 94X",)
+import subprocess, os
+version = subprocess.check_output(['git',
+    '-C', os.path.expandvars('$CMSSW_BASE/src'), 'describe', '--tags'])
+if version == '':
+    version = 'no git info'
+process.HiForest.HiForestVersion = cms.string(version)
+
+#####################################################################################
+# Input source
+#####################################################################################
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+	'file:/afs/cern.ch/work/r/rbi/public/forest/RunIIpp5Spring18DR_DYJetsToLL_MLL-50_TuneCP5_5020GeV-amcatnloFXFX-pythia8_AODSIM.root'
+    )
+)
+
+# Number of events we want to process, -1 = all events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(8))
+
+#####################################################################################
+# Load Global Tag, Geometry, etc.
+#####################################################################################
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_forppRef5TeV', '')
+process.HiForest.GlobalTagLabel = process.GlobalTag.globaltag
+
+process.GlobalTag.toGet.extend([
+    cms.PSet(record = cms.string("BTagTrackProbability3DRcd"),
+             tag = cms.string("JPcalib_MC94X_2017pp_v2"),
+             connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+
+         )
+      ])
+
+#####################################################################################
+# Define tree output
+#####################################################################################
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("HiForestAOD.root"))
+
+#####################################################################################
+# Additional Reconstruction and Analysis: Main Body
+#####################################################################################
+
+#############################
+# Jets
+#############################
+process.load("HeavyIonsAnalysis.JetAnalysis.fullJetSequence_pp_mc_cff")
+
+# Use this version for JEC
+# process.load("HeavyIonsAnalysis.JetAnalysis.fullJetSequence_pp_jec_cff")
+
+from HeavyIonsAnalysis.Configuration.CommonFunctions_cff import overrideJEC_MC_pp5020_2017  
+process = overrideJEC_MC_pp5020_2017(process)
+#####################################################################################
+
+############################
+# Event Analysis
+############################
+# use data version to avoid PbPb MC
+process.load('HeavyIonsAnalysis.EventAnalysis.hievtanalyzer_data_cfi')
+process.hiEvtAnalyzer.Vertex = cms.InputTag("offlinePrimaryVertices")
+process.hiEvtAnalyzer.doCentrality = cms.bool(False)
+process.hiEvtAnalyzer.doEvtPlane = cms.bool(False)
+process.hiEvtAnalyzer.doMC = cms.bool(True) # general MC info
+process.hiEvtAnalyzer.doHiMC = cms.bool(False) # HI specific MC info
+
+process.load('HeavyIonsAnalysis.EventAnalysis.hltanalysis_cff')
+process.load('HeavyIonsAnalysis.EventAnalysis.hltobject_cfi')
+process.load('HeavyIonsAnalysis.EventAnalysis.l1object_cfi')
+
+process.load('HeavyIonsAnalysis.JetAnalysis.HiGenAnalyzer_cfi')
+process.HiGenParticleAna.genParticleSrc = cms.untracked.InputTag("genParticles")
+process.HiGenParticleAna.doHI = False
+
+process.load('HeavyIonsAnalysis.EventAnalysis.runanalyzer_cff')
+
+process.load("HeavyIonsAnalysis.JetAnalysis.pfcandAnalyzer_pp_cfi")
+process.pfcandAnalyzer.skipCharged      = False
+process.pfcandAnalyzer.pfPtMin          = 0
+process.pfcandAnalyzer.pfCandidateLabel = cms.InputTag("particleFlow")
+process.pfcandAnalyzer.doVS             = cms.untracked.bool(False)
+process.pfcandAnalyzer.doUEraw_         = cms.untracked.bool(False)
+process.pfcandAnalyzer.genLabel         = cms.InputTag("genParticles")
+
+#####################################################################################
+
+#########################
+# Track Analyzer
+#########################
+process.load('HeavyIonsAnalysis.JetAnalysis.ExtraTrackReco_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.TrkAnalyzers_cff')
+
+# Use this instead for track corrections
+# process.load('HeavyIonsAnalysis.JetAnalysis.TrkAnalyzers_Corr_cff')
+
+#####################################################################################
+
+#####################
+# photons
+######################
+process.load('HeavyIonsAnalysis.PhotonAnalysis.ggHiNtuplizer_cfi')
+
+####################################################################################
+
+#####################
+# Electron ID
+#####################
+
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format to be processed
+# DataFormat.AOD or DataFormat.MiniAOD
+dataFormat = DataFormat.AOD
+switchOnVIDElectronIdProducer(process, dataFormat)
+
+# define which IDs we want to produce. https://twiki.cern.ch/twiki/bin/viewauth/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_7_4
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff']
+
+# add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+
+#####################################################################################
+
+#########################
+# Main analysis list
+#########################
+
+process.ana_step = cms.Path(
+    process.hltanalysis *
+    process.hiEvtAnalyzer *
+    #process.hltobject +
+    # process.l1object +
+    #process.HiGenParticleAna*
+    process.genJetSequence +
+    process.jetSequence +
+    # Should be added in the path for VID module
+    # process.egmGsfElectronIDSequence +
+    #process.ggHiNtuplizer +
+    #process.ggHiNtuplizerGED +
+    #process.pfcandAnalyzer +
+    process.HiForest #+
+    #process.trackSequencesPP +
+    #process.runAnalyzer
+)
+
+#####################################################################################
+
+#########################
+# Event Selection
+#########################
+
+process.load('HeavyIonsAnalysis.JetAnalysis.EventSelection_cff')
+process.pHBHENoiseFilterResultProducer = cms.Path(process.HBHENoiseFilterResultProducer)
+process.HBHENoiseFilterResult = cms.Path(process.fHBHENoiseFilterResult)
+process.HBHENoiseFilterResultRun1 = cms.Path(process.fHBHENoiseFilterResultRun1)
+process.HBHENoiseFilterResultRun2Loose = cms.Path(process.fHBHENoiseFilterResultRun2Loose)
+process.HBHENoiseFilterResultRun2Tight = cms.Path(process.fHBHENoiseFilterResultRun2Tight)
+process.HBHEIsoNoiseFilterResult = cms.Path(process.fHBHEIsoNoiseFilterResult)
+
+process.PAprimaryVertexFilter = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"),
+    filter = cms.bool(True), # otherwise it won't filter the events
+)
+
+process.NoScraping = cms.EDFilter("FilterOutScraping",
+    applyfilter = cms.untracked.bool(True),
+    debugOn = cms.untracked.bool(False),
+    numtrack = cms.untracked.uint32(10),
+    thresh = cms.untracked.double(0.25)
+)
+
+process.pPAprimaryVertexFilter = cms.Path(process.PAprimaryVertexFilter)
+process.pBeamScrapingFilter=cms.Path(process.NoScraping)
+
+process.load("HeavyIonsAnalysis.VertexAnalysis.PAPileUpVertexFilter_cff")
+process.pVertexFilterCutG = cms.Path(process.pileupVertexFilterCutG)
+process.pVertexFilterCutGloose = cms.Path(process.pileupVertexFilterCutGloose)
+process.pVertexFilterCutGtight = cms.Path(process.pileupVertexFilterCutGtight)
+process.pVertexFilterCutGplus = cms.Path(process.pileupVertexFilterCutGplus)
+process.pVertexFilterCutE = cms.Path(process.pileupVertexFilterCutE)
+process.pVertexFilterCutEandG = cms.Path(process.pileupVertexFilterCutEandG)
+
+process.pAna = cms.EndPath(process.skimanalysis)
+
+# Customization
+
+#gen reclustring                                                                                                                                                                                                                                                                                                                                          
+process.genParticlesForJets.storeDKPi = cms.untracked.bool(True)
+
+#################### D/B finder ################# 
+AddCaloMuon = False 
+runOnMC = True ## !!
+HIFormat = False 
+UseGenPlusSim = False 
+VtxLabel = "offlinePrimaryVerticesWithBS" 
+TrkLabel = "generalTracks" 
+useL1Stage2 = True
+HLTProName = "HLT"
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X 
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel, useL1Stage2, HLTProName)
+process.Dfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
+process.Dfinder.makeDntuple = cms.bool(True)
+process.Dfinder.tkPtCut = cms.double(0.5) # before fit
+process.Dfinder.tkEtaCut = cms.double(2.5) # before fit
+process.Dfinder.dPtCut = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0) # before fit
+process.Dfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05)
+process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
+process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+process.dfinder = cms.Path(process.DfinderSequence)
+
+
+
+###############################
+import FWCore.ParameterSet.VarParsing as VarParsing
+ivars = VarParsing.VarParsing('analysis')
+
+ivars.maxEvents = 100
+ivars.outputFile='HiForestAOD.root'
+ivars.inputFiles='/store/himc/RunIIpp5Spring18DR/QCDPhoton_pThat-50_TuneCP5_5p02TeV_pythia8/AODSIM/94X_mc2017_realistic_forppRef5TeV-v2/70000/3CAE9C09-6D11-EA11-BF28-3417EBE649DE.root'
+ivars.parseArguments() # get and parse the command line arguments
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(ivars.inputFiles)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(ivars.maxEvents)
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(ivars.outputFile))
+

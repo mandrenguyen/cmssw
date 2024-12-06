99d16aa42a86614aa84600f8bd9d35e85b646d44
mandrenguyen <Matthew.Nguyen@cer.ch>
2024-12-06 09:55:20 +0100

    modify cfg for run2 MC

--- HeavyIonsAnalysis/Configuration/test/aggregate_MC.py
+++ HeavyIonsAnalysis/Configuration/test/aggregate_MC.py
@@ -2,9 +2,15 @@
 # Input: miniAOD
 # Type: mc
 
+doRun2 = True
 import FWCore.ParameterSet.Config as cms
-from Configuration.Eras.Era_Run3_pp_on_PbPb_2023_cff import Run3_pp_on_PbPb_2023
-process = cms.Process('HiForest', Run3_pp_on_PbPb_2023)
+if doRun2:
+    from Configuration.Eras.Era_Run2_2018_pp_on_AA_cff import Run2_2018_pp_on_AA
+    from Configuration.ProcessModifiers.run2_miniAOD_pp_on_AA_103X_cff import run2_miniAOD_pp_on_AA_103X
+    process = cms.Process('HiForest', Run2_2018_pp_on_AA,run2_miniAOD_pp_on_AA_103X)
+else:
+    from Configuration.Eras.Era_Run3_pp_on_PbPb_2023_cff import Run3_pp_on_PbPb_2023
+    process = cms.Process('HiForest', Run3_pp_on_PbPb_2023)
 
 ###############################################################################
 
@@ -21,10 +27,12 @@ process.source = cms.Source("PoolSource",
         '/store/group/phys_heavyions/jviinika/PythiaHydjetRun3_5p36TeV_dijet_ptHat15_100kEvents_miniAOD_2023_08_30/PythiaHydjetDijetRun3/PythiaHydjetRun3_dijet_ptHat15_5p36TeV_miniAOD/230830_165931/0000/pythiaHydjet_miniAOD_11.root'
     ),
 )
+if doRun2:
+    process.source.fileNames = '/store/himc/HINPbPbSpring21MiniAOD/Bjet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/MINIAODSIM/FixL1CaloGT_New_Release_112X_upgrade2018_realistic_HI_v9-v1/260000/6700a2b8-9c0d-4e1a-a774-2463e1e57785.root'
 
 # number of events to process, set to -1 to process all events
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(100)
     )
 
 ###############################################################################
@@ -47,6 +55,14 @@ process.GlobalTag.toGet.extend([
              connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
          )
 ])
+if doRun2:
+    process.GlobalTag.toGet.extend([
+        cms.PSet(record = cms.string("GEMRecoGeometryRcd"),
+                 tag = cms.string("GEMRECO_Geometry_131DD4hepV1_mc_v2"),
+                 connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+             )
+    ])
+
 
 
 ###############################################################################
@@ -231,4 +247,4 @@ process.pAna = cms.EndPath(process.skimanalysis)
 process.patJetsAK2PFUnsubJets.addBTagInfo = True
 process.patJetsAK2PFUnsubJets.addTagInfos = True
 process.patJetsAK2PFUnsubJets.tagInfoSources = cms.VInputTag(["pfInclusiveSecondaryVertexFinderTagInfos","pfImpactParameterTagInfos"])
-
+process.patJetsAK2PFUnsubJets.addDiscriminators = False
